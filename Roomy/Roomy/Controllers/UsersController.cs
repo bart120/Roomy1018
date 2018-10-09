a62d67eeb012fdb3ae761ee70b7dcbffcf2160a5
Vincent <vleclerc@inow.fr>
2018-10-09 14:28:00 +0200

    add view create for users

---
+++ Roomy/Roomy/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roomy.Models;
+
+namespace Roomy.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+           
+            return View(user);
+        }
+    }
+}
--- Roomy/Roomy/Models/User.cs
+++ Roomy/Roomy/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,25 @@ namespace Roomy.Models
 {
     public class User
     {
+        [Display(Name = "Nom de famille", ShortName = "Nom", Prompt = "Nom de l'utilisateur")]
         public string Lastname { get; set; }
 
+        [Display(Name = "Prénom")]
         public string Firstname { get; set; }
 
+        [Display(Name = "Adresse mail")]
         public string Mail { get; set; }
 
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Confirmation du mot de passe")]
+        [DataType(DataType.Password)]
         public string ConfirmedPassword { get; set; }
 
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
     }
 }
